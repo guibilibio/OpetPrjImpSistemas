38f7511b6b8969444b70ecbc19383477624f37b0
Guilherme Bilibio <gui-bilibio@hotmail.com>
2021-04-12 15:56:56 -0300

    Commit 12-04-2021

--- src/main/java/opet/edu/br/opetApp/models/Pessoa.java
+++ src/main/java/opet/edu/br/opetApp/models/Pessoa.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
